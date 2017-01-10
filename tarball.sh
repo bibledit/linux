3299dd999cd396919c4757cd0fbb0e5b0f3537b6
Teus Benschop <teusjannette@gmail.com>
2017-01-10 13:55:25 +0100

    Easier way to create the tarball

--- tarball.sh
+++ tarball.sh
@@ -28,15 +28,8 @@ make distclean
 
 # Update the source for the configuration.
 sed -i.bak 's/ENABLELINUX=no/ENABLELINUX=yes/g' configure.ac
+sed -i.bak 's/dnl linux //g' configure.ac
 sed -i.bak 's/.*Tag8.*/AC_DEFINE([HAVE_LINUX], [1], [Enable installation on Linux])/g' configure.ac
-
-sed -i.bak 's/.*Tag1.*/PKG_CHECK_MODULES(GTK, gtk+-3.0,,AC_MSG_ERROR(Gtk3 development version is needed.))/g' configure.ac
-sed -i.bak 's/.*Tag2.*/AC_SUBST(GTK_LIBS)/g' configure.ac
-sed -i.bak 's/.*Tag3.*/AC_SUBST(GTK_CFLAGS)/g' configure.ac
-
-sed -i.bak 's/.*Tag4.*/PKG_CHECK_MODULES(WEBKIT2GTK, webkit2gtk-4.0,,AC_MSG_ERROR(Webkit2Gtk development version is needed.))/g' configure.ac
-sed -i.bak 's/.*Tag5.*/AC_SUBST(WEBKIT2GTK_LIBS)/g' configure.ac
-sed -i.bak 's/.*Tag6.*/AC_SUBST(WEBKIT2GTK_CFLAGS)/g' configure.ac
 
 
 # Update the source for the Makefile.
@@ -44,8 +37,6 @@ sed -i.bak 's/server unittest/bibledit/g' Makefile.am
 sed -i.bak 's/bin_PROGRAMS/noinst_PROGRAMS/g' Makefile.am
 sed -i.bak 's/server_/bibledit_/g' Makefile.am
 sed -i.bak '/unittest/d' Makefile.am
-sed -i.bak '/AM_CXXFLAGS =/ s/$/ $(GTK_CFLAGS) $(WEBKIT2GTK_CFLAGS)/' Makefile.am
-sed -i.bak '/LIBS =/ s/$/ $(GTK_LIBS) $(WEBKIT2GTK_LIBS)/' Makefile.am
 sed -i.bak 's/bible bibledit/bible/g' Makefile.am
 sed -i.bak '/EXTRA_DIST/ s/$/ *.desktop *.xpm *.sh *.1/' Makefile.am
 echo '' >> Makefile.am
