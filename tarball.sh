82e41b0f3ff46850d6e552c80802cf92475ab25a
Teus Benschop <teusjannette@gmail.com>
2017-01-22 17:36:28 +0100

    Updating script to produce tarball
    The update is in preparation for a canonical Linux tarball from which
    Linux packages can be created.
    
    https://github.com/bibledit/bibledit/issues/342

--- tarball.sh
+++ tarball.sh
@@ -32,23 +32,44 @@ sed -i.bak 's/# linux //g' configure.ac
 sed -i.bak 's/.*Tag8.*/AC_DEFINE([HAVE_LINUX], [1], [Enable installation on Linux])/g' configure.ac
 
 
-# Update the source for the Makefile.
-sed -i.bak 's/server unittest/bibledit/g' Makefile.am
-sed -i.bak 's/bin_PROGRAMS/noinst_PROGRAMS/g' Makefile.am
+# Do not build the unit tests and the generator.
+# Rename binary 'server' to 'bibledit'.
+sed -i.bak 's/server unittest generate/bibledit/g' Makefile.am
 sed -i.bak 's/server_/bibledit_/g' Makefile.am
 sed -i.bak '/unittest/d' Makefile.am
+sed -i.bak '/generate_/d' Makefile.am
+
+
+sed -i.bak 's/bin_PROGRAMS/noinst_PROGRAMS/g' Makefile.am
+
+
+# Update what to distribute.
 sed -i.bak 's/bible bibledit/bible/g' Makefile.am
 sed -i.bak '/EXTRA_DIST/ s/$/ *.desktop *.xpm *.sh *.1/' Makefile.am
+
+
+# Install the man pages.
 echo '' >> Makefile.am
 echo 'man_MANS = *.1' >> Makefile.am
+
+
+# Install the desktop shortcut.
 echo '' >> Makefile.am
 echo 'applicationdir = $(datadir)/applications' >> Makefile.am
 echo 'application_DATA = bibledit.desktop' >> Makefile.am
+
+
+# Install the application icon.
 echo '' >> Makefile.am
 echo 'appicondir = $(datadir)/pixmaps' >> Makefile.am
 echo 'appicon_DATA = bibledit.xpm' >> Makefile.am
+
+
+# Install the startup script.
 echo '' >> Makefile.am
 echo 'bin_SCRIPTS = bibledit.sh' >> Makefile.am
+
+
 # Remove consecutive blank lines.
 sed -i.bak '/./,/^$/!d' Makefile.am
 
@@ -65,11 +86,9 @@ rm dev
 rm *.bak
 
 
+# Clean everything up and create distribution tarball.
 ./reconfigure
-
 make distclean
-
 ./configure
-
 make dist
 
