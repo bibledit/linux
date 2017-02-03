8cfb4b72761bfb8eb56cc0d4f1b8142a46dcbf46
Teus Benschop <teusjannette@gmail.com>
2017-02-03 18:54:37 +0100

    Launcher on Linux #448
    https://github.com/bibledit/bibledit/issues/448

--- Makefile.mk
+++ Makefile.mk
@@ -3,8 +3,11 @@ man_MANS = bibledit.1
 applicationdir = $(datadir)/applications
 application_DATA = bibledit.desktop
 
-appicondir = $(datadir)/pixmaps
-appicon_DATA = bibledit.xpm
+pixmapsdir = $(datadir)/pixmaps
+pixmaps_DATA = bibledit.xpm
+
+iconsdir = $(datadir)/icons
+icons_DATA = bibledit.png
 
 install-data-hook:
 	rsync --archive --delete --exclude "*.h" --exclude "*.c" --exclude "*.cpp" --exclude "*.o" --exclude "*.a" --exclude "*.deps*" --exclude "*autom4te.cache*" --exclude ".dirstamp" --exclude ".DS_Store" --exclude "bibledit" --exclude "xcode*" --exclude "unittest*" $(srcdir) $(DESTDIR)$(pkgdatadir)
