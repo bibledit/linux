23ffb18adca2da2c7663c5ab5749efb6464dc09f
Teus Benschop <teusjannette@gmail.com>
2017-02-22 11:17:25 +0100

    Updated debian builder
    * Produce debian tarball.
    * Fix lintian errors and warnings.
    * More robust file locator.
    https://github.com/bibledit/bibledit/issues/342

--- debian.sh
+++ debian.sh
@@ -33,61 +33,101 @@ if [ $? -ne 0 ]; then exit; fi
 
 
 TMPLINUX=/tmp/bibledit-linux
-echo Works with the tarball supposed to be already in $TMPLINUX.
+echo It supposes a tarball to be already in $TMPLINUX.
+
+
+TMPDEBIAN=/tmp/bibledit-debian
+echo Unpack the tarball in $TMPLINUX.
+rm -rf $TMPDEBIAN
+mkdir $TMPDEBIAN
+cd $TMPDEBIAN
+if [ $? -ne 0 ]; then exit; fi
+tar xf $TMPLINUX/bibledit*gz
+if [ $? -ne 0 ]; then exit; fi
+cd bibledit*
+if [ $? -ne 0 ]; then exit; fi
+
+
+# The script unpacks the existing Bibledit Linux tarball,
+# modifies it, and repacks it into a Debian tarball.
+# The reason for doing so is that the Debian builder would otherwise notice
+# differences between the supplied tarball and the modified source.
+# dpkg-source: error: aborting due to unexpected upstream changes
+# Another reason is that in this way it does not need to generate patches in the 'debian' folder.
+
+
+echo Link with the system-provided mbed TLS library.
+# Fix for lintian error "embedded-library usr/bin/bibledit: mbedtls":
+# * Remove mbedtls from the list of sources to compile.
+# * Add -lmbedtls to the linker flags instead.
+sed -i.bak '/mbedtls\//d' Makefile.am
+if [ $? -ne 0 ]; then exit; fi
+sed -i.bak 's/# debian//g' Makefile.am
+if [ $? -ne 0 ]; then exit; fi
+rm *.bak
+
+
+echo Reconfiguring the source.
+./reconfigure
+if [ $? -ne 0 ]; then exit; fi
+rm -rf autom4te.cache
+if [ $? -ne 0 ]; then exit; fi
+
+
+echo Remove extra license files.
+# Fix for the lintian warnings "extra-license-file".
+find . -name COPYING -delete
+if [ $? -ne 0 ]; then exit; fi
+find . -name LICENSE -delete
+if [ $? -ne 0 ]; then exit; fi
+
+
+echo Remove extra font files.
+# Fix for the lintian warning "duplicate-font-file".
+rm fonts/SILEOT.ttf
+if [ $? -ne 0 ]; then exit; fi
+
+
+echo Configure and clean the source.
+./configure
+if [ $? -ne 0 ]; then exit; fi
+make distclean
+if [ $? -ne 0 ]; then exit; fi
+
+
+echo Create updated tarball for Debian.
+cd $TMPDEBIAN
+TARDIR=`ls`
+tar czf $TARDIR.tar.gz $TARDIR
+if [ $? -ne 0 ]; then exit; fi
 
 
 echo Clean the Debian builder and copy the tarball to it.
 ssh $DEBIANSID "rm -rf bibledit*"
 if [ $? -ne 0 ]; then exit; fi
-scp $TMPLINUX/*.gz $DEBIANSID:.
+scp $TMPDEBIAN/*.gz $DEBIANSID:.
 if [ $? -ne 0 ]; then exit; fi
 
 
-echo Unpack and remove the tarball in Debian.
+echo Rename the source tarball to the non-native scheme.
+ssh $DEBIANSID "rename 's/-/_/g' bibledit*gz"
+if [ $? -ne 0 ]; then exit; fi
+ssh $DEBIANSID "rename 's/tar/orig.tar/g' bibledit*gz"
+if [ $? -ne 0 ]; then exit; fi
+
+
+echo Unpack the tarball in Debian.
 ssh $DEBIANSID "tar xf bibledit*gz"
-if [ $? -ne 0 ]; then exit; fi
-ssh $DEBIANSID "rm bibledit*gz"
-if [ $? -ne 0 ]; then exit; fi
-
-
-echo Link with the system-provided mbed TLS library.
-# A fix for lintian error "embedded-library usr/bin/bibledit: mbedtls"
-# is to remove mbedtls from the list of sources to compile
-# and to add -lmbedtls to the linker flags instead.
-ssh -tt $DEBIANSID "cd bibledit*; sed -i.bak '/mbedtls\//d' Makefile.am"
-if [ $? -ne 0 ]; then exit; fi
-ssh -tt $DEBIANSID "cd bibledit*; sed -i.bak 's/# debian//g' Makefile.am"
-if [ $? -ne 0 ]; then exit; fi
-
-
-echo Reconfiguring the source.
-ssh -tt $DEBIANSID "cd bibledit*; ./reconfigure"
-if [ $? -ne 0 ]; then exit; fi
-ssh -tt $DEBIANSID "cd bibledit*; rm -rf autom4te.cache"
-if [ $? -ne 0 ]; then exit; fi
-
-
-echo Remove extra license files.
-# Fix the lintian warnings "extra-license-file".
-ssh -tt $DEBIANSID "find . -name COPYING -delete"
-if [ $? -ne 0 ]; then exit; fi
-ssh -tt $DEBIANSID "find . -name LICENSE -delete"
-if [ $? -ne 0 ]; then exit; fi
-
-
-echo Remove extra font files.
-# Fix the lintian warning "duplicate-font-file".
-ssh -tt $DEBIANSID "cd bibledit*; rm fonts/SILEOT.ttf"
 if [ $? -ne 0 ]; then exit; fi
 
 
 echo Do a license check.
-ssh -tt $DEBIANSID "cd bibledit*; licensecheck --recursive --ignore debian --deb-machine *"
+ssh -tt $DEBIANSID "cd bibledit*[0-9]; licensecheck --recursive --ignore debian --deb-machine *"
 if [ $? -ne 0 ]; then exit; fi
 
 
 echo Build the Debian packages.
-ssh -tt $DEBIANSID "cd bibledit*; debuild -us -uc"
+ssh -tt $DEBIANSID "cd bibledit*[0-9]; debuild -us -uc"
 if [ $? -ne 0 ]; then exit; fi
 
 
@@ -96,12 +136,12 @@ ssh -tt $DEBIANSID "lintian --pedantic bibledit*changes"
 if [ $? -ne 0 ]; then exit; fi
 
 
-echo Remove the generated build artifacts.
-ssh $DEBIANSID "rm bibledit*"
+# echo Remove the generated build artifacts.
+# ssh $DEBIANSID "rm bibledit*"
 
 
 echo Build the Debian package in a chroot.
-ssh -tt $DEBIANSID "cd bibledit*; sbuild"
+ssh -tt $DEBIANSID "cd bibledit*[0-9]; sbuild"
 if [ $? -ne 0 ]; then exit; fi
 
 
