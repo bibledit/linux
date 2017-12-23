f4d1ea40172d5d8b6920784beb85ddab3bdd38ec
Teus Benschop <teusjannette@gmail.com>
2017-12-23 17:59:47 +0100

    Fix: not rule to make target bb445x48...
    https://github.com/bibledit/cloud/issues/120

--- Makefile.mk
+++ Makefile.mk
@@ -2,7 +2,7 @@ applicationdir = $(datadir)/applications
 application_DATA = bibledit.desktop
 
 pixmapsdir = $(datadir)/pixmaps
-pixmaps_DATA = bbe45x48.xpm
+pixmaps_DATA = bbe48x48.xpm
 
 iconsdir = $(datadir)/icons
 icons_DATA = bbe512x512.png
