ad409b6caf240f85f955da1532b0e53334f3f475
Teus Benschop <teusjannette@gmail.com>
2018-01-17 13:46:53 +0100

    Fixing non-visible icon on Ubuntu

--- Makefile.mk
+++ Makefile.mk
@@ -2,8 +2,12 @@ applicationdir = $(datadir)/applications
 application_DATA = bibledit.desktop
 
 pixmapsdir = $(datadir)/pixmaps
-pixmaps_DATA = bbe48x48.xpm
+pixmaps_DATA = bbe48x48.xpm bbe512x512.png
 
 iconsdir = $(datadir)/icons
-icons_DATA = bbe512x512.png
+icons_DATA = bbe48x48.xpm bbe512x512.png
 
+## It should be sufficient to put the icon for the .desktop file
+## in the icons folder only,
+## but a Debian package somehow did not install those.
+## That is why the icon is also put in pixmaps folder.
